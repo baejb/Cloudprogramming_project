c7ec807df33680003d88833b51aa6e706a86aace
Baejb <bjb6478@naver.com>
2022-06-09 02:55:16 +0900

    diary에 Post모델 생성

--- djangodiary/settings.py
+++ djangodiary/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diary',
+    'todolist',
+    'diary_single_pages',
+    'todolist_single_pages',
 ]
 
 MIDDLEWARE = [
@@ -105,13 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
---
+++ todolist_single_pages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TodolistSinglePagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'todolist_single_pages'
