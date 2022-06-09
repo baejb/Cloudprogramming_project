fb83d0cdf66edab572c96f4dff52a090c0f448cd
Baejb <bjb6478@naver.com>
2022-06-09 13:03:52 +0900

    일기장 목록 뷰 생성

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk') #모든 포스트를 가져옴
+
+    return render(
+        request,
+        'diary/index.html',
+        {
+            'posts': posts,
+        }
+    )
